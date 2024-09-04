2ed28d64c94dd2a7d107768aa9e491ed7a55e63f
yuanzhen <yuanzhenwork@qq.com>
2024-09-04 16:13:03 +0800

    feat: 移除了 App.js 中的 role="list" 属性

--- src/App.js
+++ src/App.js
@@ -60,7 +60,6 @@ function App(props) {
       </div>
       <h2 id="list-heading">{taskList.length} tasks remaining</h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         {taskList}
